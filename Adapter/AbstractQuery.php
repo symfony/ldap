symfony/symfony

[Ldap] Allow search scoping

src/Symfony/Component/Ldap/Adapter/AbstractQuery.php
@@ -34,8 +34,11 @@ abstract class AbstractQuery implements QueryInterface
             'timeout' => 0,
             'deref' => static::DEREF_NEVER,
             'attrsOnly' => 0,
+            'scope' => static::SCOPE_SUBTREE,
         ));
         $resolver->setAllowedValues('deref', array(static::DEREF_ALWAYS, static::DEREF_NEVER, static::DEREF_FINDING, static::DEREF_SEARCHING));
+        $resolver->setAllowedValues('scope', array(static::SCOPE_BASE, static::SCOPE_ONELEVEL, static::SCOPE_SUBTREE));
+
         $resolver->setNormalizer('filter', function (Options $options, $value) {
             return is_array($value) ? $value : array($value);
         });

src/Symfony/Component/Ldap/Tests/Adapter/ExtLdap/AdapterTest.php
@@ -13,6 +13,7 @@ namespace Symfony\Component\Ldap\Tests;
 
 use Symfony\Component\Ldap\Adapter\ExtLdap\Adapter;
 use Symfony\Component\Ldap\Adapter\ExtLdap\Collection;
+use Symfony\Component\Ldap\Adapter\ExtLdap\Query;
 use Symfony\Component\Ldap\Entry;
 use Symfony\Component\Ldap\LdapInterface;
 
@@ -64,5 +65,38 @@ class AdapterTest extends LdapTestCase
         $this->assertInstanceOf(Entry::class, $entry);
         $this->assertEquals(array('Fabien Potencier'), $entry->getAttribute('cn'));
         $this->assertEquals(array('fabpot@symfony.com', 'fabien@potencier.com'), $entry->getAttribute('mail'));
+    }
+
+    public function testLdapQueryScopeBase()
+    {
+        $ldap = new Adapter($this->getLdapConfig());
+
+        $ldap->getConnection()->bind('cn=admin,dc=symfony,dc=com', 'symfony');
+
+        $query = $ldap->createQuery('cn=Fabien Potencier,dc=symfony,dc=com', '(objectclass=*)', array(
+           'scope' => Query::SCOPE_BASE,
+        ));
+        $result = $query->execute();
+
+        $entry = $result[0];
+        $this->assertEquals($result->count(), 1);
+        $this->assertEquals(array('Fabien Potencier'), $entry->getAttribute('cn'));
+    }
+
+    public function testLdapQueryScopeOneLevel()
+    {
+        $ldap = new Adapter($this->getLdapConfig());
+
+        $ldap->getConnection()->bind('cn=admin,dc=symfony,dc=com', 'symfony');
+
+        $one_level_result = $ldap->createQuery('ou=Components,dc=symfony,dc=com', '(objectclass=*)', array(
+            'scope' => Query::SCOPE_ONELEVEL,
+        ))->execute();
+
+        $subtree_count = $ldap->createQuery('ou=Components,dc=symfony,dc=com', '(objectclass=*)')->execute()->count();
+
+        $this->assertNotEquals($one_level_result->count(), $subtree_count);
+        $this->assertEquals($one_level_result->count(), 1);
+        $this->assertEquals($one_level_result[0]->getAttribute('ou'), array('Ldap'));
     }
 }

symfony/ldap

Adapter/AbstractQuery.php
@@ -34,8 +34,11 @@ abstract class AbstractQuery implements QueryInterface
             'timeout' => 0,
             'deref' => static::DEREF_NEVER,
             'attrsOnly' => 0,
+            'scope' => static::SCOPE_SUB,
         ));
         $resolver->setAllowedValues('deref', array(static::DEREF_ALWAYS, static::DEREF_NEVER, static::DEREF_FINDING, static::DEREF_SEARCHING));
+        $resolver->setAllowedValues('scope', array(static::SCOPE_BASE, static::SCOPE_ONE, static::SCOPE_SUB));
+
         $resolver->setNormalizer('filter', function (Options $options, $value) {
             return is_array($value) ? $value : array($value);
         });

Tests/Adapter/ExtLdap/AdapterTest.php
@@ -13,6 +13,7 @@ namespace Symfony\Component\Ldap\Tests;
 
 use Symfony\Component\Ldap\Adapter\ExtLdap\Adapter;
 use Symfony\Component\Ldap\Adapter\ExtLdap\Collection;
+use Symfony\Component\Ldap\Adapter\ExtLdap\Query;
 use Symfony\Component\Ldap\Entry;
 use Symfony\Component\Ldap\LdapInterface;
 
@@ -64,5 +65,38 @@ class AdapterTest extends LdapTestCase
         $this->assertInstanceOf(Entry::class, $entry);
         $this->assertEquals(array('Fabien Potencier'), $entry->getAttribute('cn'));
         $this->assertEquals(array('fabpot@symfony.com', 'fabien@potencier.com'), $entry->getAttribute('mail'));
+    }
+
+    public function testLdapQueryScopeBase()
+    {
+        $ldap = new Adapter($this->getLdapConfig());
+
+        $ldap->getConnection()->bind('cn=admin,dc=symfony,dc=com', 'symfony');
+
+        $query = $ldap->createQuery('cn=Fabien Potencier,dc=symfony,dc=com', '(objectclass=*)', array(
+           'scope' => Query::SCOPE_BASE,
+        ));
+        $result = $query->execute();
+
+        $entry = $result[0];
+        $this->assertEquals($result->count(), 1);
+        $this->assertEquals(array('Fabien Potencier'), $entry->getAttribute('cn'));
+    }
+
+    public function testLdapQueryScopeOneLevel()
+    {
+        $ldap = new Adapter($this->getLdapConfig());
+
+        $ldap->getConnection()->bind('cn=admin,dc=symfony,dc=com', 'symfony');
+
+        $one_level_result = $ldap->createQuery('ou=Components,dc=symfony,dc=com', '(objectclass=*)', array(
+            'scope' => Query::SCOPE_ONE,
+        ))->execute();
+
+        $subtree_count = $ldap->createQuery('ou=Components,dc=symfony,dc=com', '(objectclass=*)')->execute()->count();
+
+        $this->assertNotEquals($one_level_result->count(), $subtree_count);
+        $this->assertEquals($one_level_result->count(), 1);
+        $this->assertEquals($one_level_result[0]->getAttribute('ou'), array('Ldap'));
     }
 }