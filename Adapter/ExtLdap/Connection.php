f34c07de3e4654d30f96b5ea8e28ec6583f899c6
Ousama Ben Younes <benyounes.ousama@gmail.com>
2026-05-01 22:32:50 +0000

    [Ldap] Cast default network_timeout to int
    `ini_get('default_socket_timeout')` returns a string. PHP 8.4's ext-ldap
    strictly requires an int for `LDAP_OPT_NETWORK_TIMEOUT`, so binding fails
    with `Could not set value "60" for option "network_timeout"` whenever the
    fallback is used (i.e. the user did not set `network_timeout` explicitly).
    
    Casting the value to int when storing the default fixes the regression on
    PHP 8.4 while remaining a no-op on prior versions.

--- Adapter/ExtLdap/Connection.php
+++ Adapter/ExtLdap/Connection.php
@@ -135,7 +135,7 @@ class Connection extends AbstractConnection
             }
 
             if (!isset($parent['network_timeout'])) {
-                $options->setDefault('network_timeout', \ini_get('default_socket_timeout'));
+                $options->setDefault('network_timeout', (int) \ini_get('default_socket_timeout'));
             }
 
             $options->setDefaults([
